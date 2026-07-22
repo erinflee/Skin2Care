626712d6c3da43cecbdd9a3a397ebafab11e1529
erinlee316 <127688889+erinflee@users.noreply.github.com>
2026-07-22 10:38:52 -0700

    add function that checks if s3 bucket is configured or not

--- s3_utils.py
+++ s3_utils.py
@@ -4,10 +4,19 @@
 # 41MB of binary index files to Git.
 #
 
-
 import os
 import boto3
 from pathlib import Path
 
 INDEX_FILES = ("index.faiss", "index.pkl")
+
+
+def s3_enabled():
+    """Return True when an S3 bucket is configured, False otherwise.
+
+    When False, the caller should just use the local faiss_index/ folder.
+    """
+    bucket = os.environ.get("S3_BUCKET")
+    return bool(bucket)
+
 
