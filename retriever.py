4ce1deeb5823083aa9f7cb2531a500cb427e6aed
erinlee316 <127688889+erinflee@users.noreply.github.com>
2026-07-23 21:57:22 -0700

    add product_to_text to format embeddings with only important product info

--- retriever.py
+++ retriever.py
@@ -4,3 +4,12 @@ import faiss
 import numpy as np
 from groq import Groq
 from sentence_transformers import SentenceTransformer
+
+
+
+def product_to_text(product):
+  return (
+    f"{product['product_name']} by {product['brand']}"
+    f"{product['product_details']}"
+    f"{product['ingredients']}"
+  )
